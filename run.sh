191cfc9c2c533eba5297d7329fb7380edc0397ed
Nishant Holla <nishantholla19@gmail.com>
2026-01-06 20:22:06 +0530

    Update build script

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ CC="gcc"
 LD="gcc"
 CFLAGS="-c -g -m32"
 LDFLAGS="-m32 -no-pie"
+LIBS=""
 BUILD_DIR="./build"
 
 if [ "$#" -lt 1 ]; then
@@ -22,18 +23,26 @@ if [[ ! -d "$1" ]]; then
 fi
 
 if [[ "$(realpath "$1")" == "$(realpath "./10.dynamic_libraries/")" ]]; then
-  # LDFLAGS+=" -Llib -lfactorial -Wl,-rpath,./lib"
-  LDFLAGS+=" -Llib -lfactorial"
+  # LDFLAGS+=" -Llib -Wl,-rpath,./lib"
+  LDFLAGS+=" -Llib"
+  LIBS+=" -lfactorial"
+fi
+
+if [[ "$(realpath "$1")" == "$(realpath "./14.raylib_test/")" ]]; then
+  LDFLAGS+=" -Llib"
+  LIBS+=" -lraylib -lm -ldl -lpthread -lX11 -lGL"
 fi
 
 rm -f $BUILD_DIR/*.o *.out
 
 mkdir -p $BUILD_DIR
 
-for file in $1/*.[sc]; do
+cd $1
+for file in *.[sc]; do
   base=`basename "$file" | sed 's/\.\(c\|s\)$//'`
-  $CC $CFLAGS $file -o $BUILD_DIR/$base.o
+  $CC $CFLAGS $file -o ../$BUILD_DIR/$base.o
 done
+cd ..
 
-$LD $LDFLAGS $BUILD_DIR/*.o -o $1.out
+$LD $LDFLAGS $BUILD_DIR/*.o -o $1.out $LIBS
 LD_LIBRARY_PATH="./lib" ./$1.out "${@:2}"
